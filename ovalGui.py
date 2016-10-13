ea1ee2837ad66a87f59c8bab48b93d59766b7522
Chiron <arnaud.chiron@cern.ch>
2016-10-13 17:14:42 +0200

    Version 0.0.3
    The choice window was removed and included into the first window. More,
    Next/Previous buttons can be used for the Release/Reference choice.

--- ovalGui.py
+++ ovalGui.py
@@ -18,7 +18,7 @@ from getChoice import *
 class ovalGui(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.setWindowTitle('Validations gui v0.0.2')
+        self.setWindowTitle('Validations gui v0.0.3')
 
         self.cmsenv = env()
         self.texte = self.cmsenv.cmsAll()
@@ -33,7 +33,7 @@ class ovalGui(QWidget):
         self.working_dir_rel = os.getcwd()
         self.working_dir_ref = os.getcwd()
         
-#        self.releasesList_0 = list_search_0(self) # list of releases in https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/
+        self.releasesList_0 = list_search_0(self) # list of releases in https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/
         self.my_choice_rel_0 = "" # 
         self.my_choice_rel_1 = "" # 
         self.my_choice_ref_0 = "" # 
@@ -45,9 +45,10 @@ class ovalGui(QWidget):
         self.profondeur_rel = 0
         self.profondeur_ref = 0
 						
+        ## PART 1 ##
 		# creation du grpe Calcul
         self.QGBox1 = QGroupBox("Calcul")
-        self.QGBox1.setMaximumHeight(150)
+        self.QGBox1.setMaximumHeight(120)
         self.QGBox1.setMaximumWidth(100)
         self.radio11 = QRadioButton("FULL") # par defaut
         self.radio12 = QRadioButton("PU")
@@ -66,8 +67,8 @@ class ovalGui(QWidget):
 		# creation du grpe liste des collections
         self.QGBox31 = QGroupBox("Data Sets")
         self.QGBox32 = QGroupBox("Data Sets")
-        self.QGBox31.setMaximumHeight(150)
-        self.QGBox32.setMaximumHeight(150)
+        self.QGBox31.setMaximumHeight(120)
+        self.QGBox32.setMaximumHeight(120)
         self.QGBox31.setVisible(True)
         self.QGBox32.setVisible(False)
         self.check31 = QCheckBox("Pt10Startup_UP15")
@@ -97,8 +98,8 @@ class ovalGui(QWidget):
         
 		# creation du grpe All/None
         self.QGBoxAllNone = QGroupBox("All / None")
-        self.QGBoxAllNone.setMaximumHeight(150)
-        self.QGBoxAllNone.setMinimumHeight(150)
+        self.QGBoxAllNone.setMaximumHeight(120)
+        self.QGBoxAllNone.setMinimumHeight(120)
         self.checkAllNone1 = QRadioButton("All")
         self.checkAllNone2 = QRadioButton("None")
         self.checkAllNone1.setChecked(True)
@@ -110,6 +111,21 @@ class ovalGui(QWidget):
         vboxAllNone.addStretch(1)
         self.QGBoxAllNone.setLayout(vboxAllNone)
                 				
+		# creation des texEdit pour release/reference
+        self.QGBoxRelRef = QGroupBox("release")
+        self.QGBoxRelRef.setMaximumHeight(120)
+        self.QGBoxRelRef.setMinimumHeight(120)
+        self.QGBoxRelRef.setMinimumWidth(250)
+        
+        self.labelCombo1 = QLabel(self.trUtf8("Release"), self)
+        self.labelCombo2 = QLabel(self.trUtf8("Reference"), self)
+
+        vbox6 = QVBoxLayout()
+        vbox6.addWidget(self.labelCombo1) 
+        vbox6.addWidget(self.labelCombo2) 
+        vbox6.addStretch(1)
+        self.QGBoxRelRef.setLayout(vbox6)
+
         #Layout intermédiaire : création et peuplement des gpes radios
         self.layoutH_radio = QHBoxLayout()
         self.layoutH_radio.addWidget(self.QGBox1)
@@ -117,144 +133,104 @@ class ovalGui(QWidget):
         self.layoutH_radio.addWidget(self.QGBox32)
         self.layoutH_radio.addWidget(self.QGBoxAllNone)
         self.layoutH_radio.addStretch(1)
+        self.layoutH_radio.addWidget(self.QGBoxRelRef)
 
-        # Création du bouton Get choice !, ayant pour parent la "fenetre"
-        self.bouton3 = QPushButton(self.trUtf8("Get choice !"),self)
-        self.bouton3.setFont(QFont("Comic Sans MS", 14,QFont.Bold,True))
-        self.bouton3.setIcon(QIcon("../images/smile.png"))
-        self.connect(self.bouton3, SIGNAL("clicked()"), self.liste4) 
+        ## PART 2 ##
+		# creation du label resumé
+        self.labelResume = QLabel(self.trUtf8(self.texte), self)
+		# creation du grpe Folders paths
+        self.QGBoxResume = QGroupBox("Folders paths")
+        vbox8 = QVBoxLayout()
+        vbox8.addWidget(self.labelResume)
+        self.QGBoxResume.setLayout(vbox8)
 
+        #Layout intermédiaire : ComboBox + labelcombo
+        self.layoutV_combobox = QVBoxLayout()
+        self.layoutV_combobox.addWidget(self.QGBoxResume)
+        
+        # creation des onglets
+        self.layout_onglets = QTabWidget()
+        self.generalTab = QWidget()
+        self.generalTab.setMinimumHeight(170)
+        self.layout_onglets.insertTab(0, self.generalTab, "General")
+        #Set Layout for Tabs Pages
+        self.generalTab.setLayout(self.layoutV_combobox)   
+
+        ## BOTTOM PART ##
         # Création du bouton quitter, ayant pour parent la "fenetre"
         self.boutonQ = QPushButton(self.trUtf8("Quitter ?"),self)
         self.boutonQ.setFont(QFont("Comic Sans MS", 14,QFont.Bold,True))
         self.boutonQ.setIcon(QIcon("../images/smile.png"))
         self.connect(self.boutonQ, SIGNAL("clicked()"), qApp, SLOT("quit()"))
         
+        # créer un bouton
+        self.bouton_Next = QPushButton("Next", self)
+        self.bouton_Next.clicked.connect(self.Next_Choice)
+        self.bouton_Previous = QPushButton("Previous", self)
+        self.bouton_Previous.clicked.connect(self.Previous_Choice)
+        self.bouton_Previous.setEnabled(False) #default
+        
         #Layout intermédiaire : boutons
         self.layoutH_boutons = QHBoxLayout()
-        self.layoutH_boutons.addWidget(self.bouton3)
+        self.layoutH_boutons.addWidget(self.bouton_Previous)
+        self.layoutH_boutons.addWidget(self.bouton_Next)
         self.layoutH_boutons.addStretch(1)
         self.layoutH_boutons.addWidget(self.boutonQ)
 
-		# creation du label resumé
-        self.labelResume = QLabel(self.trUtf8(self.texte), self)
-		# creation du grpe Folders paths
-        self.QGBox8 = QGroupBox("Folders paths")
-        vbox8 = QVBoxLayout()
-        vbox8.addWidget(self.labelResume)
-        self.QGBox8.setLayout(vbox8)
-
-        #Layout intermédiaire : ComboBox + labelcombo
-        self.layoutV_combobox = QVBoxLayout()
-        self.layoutV_combobox.addWidget(self.QGBox8)
+        ## PART 3 ##
+        # release/reference
+        self.QGBox_rel0 = QGroupBox("Release list") # default
+        self.QGBox_rel0.setMinimumHeight(200)
+        self.QHL_rel = QHBoxLayout(self.QGBox_rel0)
+        self.QGBox_rel1 = QGroupBox("List")
+        self.QGBox_rel1.setMinimumWidth(180)
+        self.QGBox_rel1.setMaximumWidth(180)
+        self.vbox_rel1 = QVBoxLayout()
+        self.QLW_rel1 = QListWidget()
+        for it in self.releasesList_0:
+            item = QListWidgetItem("%s" % it)
+            self.QLW_rel1.addItem(item)
+        self.connect(self.QLW_rel1, SIGNAL("itemSelectionChanged()"),self.ItemRelRefClicked1)
+        self.vbox_rel1.addWidget(self.QLW_rel1)        
+        self.QGBox_rel1.setLayout(self.vbox_rel1)
+        self.QHL_rel.addWidget(self.QGBox_rel1)
         
-        # creation des onglets
-        self.onglets = QTabWidget()
-        self.generalTab = QWidget()
-        self.generalTab.setMinimumHeight(170)
-        self.onglets.insertTab(0, self.generalTab, "General")
-        #Set Layout for Tabs Pages
-        self.generalTab.setLayout(self.layoutV_combobox)   
+        self.QGBox_rel2 = QGroupBox("Release")
+        self.QGBox_rel2.setMinimumWidth(200)
+        self.QGBox_rel2.setMaximumWidth(200)
+        self.vbox_rel2 = QVBoxLayout()
+        self.QLW_rel2 = QListWidget()
+        item = QListWidgetItem("%s" % "")
+        self.QLW_rel2.addItem(item)
+        self.connect(self.QLW_rel2, SIGNAL("itemSelectionChanged()"),self.ItemRelRefClicked2)
+        self.vbox_rel2.addWidget(self.QLW_rel2)        
+        self.QGBox_rel2.setLayout(self.vbox_rel2)
+        self.QHL_rel.addWidget(self.QGBox_rel2)
+        
+        self.QGBox_rel3 = QGroupBox("DataSets")
+        self.QGBox_rel3.setMinimumWidth(250)
+        self.QGBox_rel3.setMaximumWidth(250)
+        self.vbox_rel3 = QVBoxLayout()
+        self.QLW_rel3 = QListWidget()
+        item = QListWidgetItem("%s" % "")
+        self.QLW_rel3.addItem(item)
+        self.connect(self.QLW_rel3, SIGNAL("itemSelectionChanged()"),self.ItemRelRefClicked3)
+        self.vbox_rel3.addWidget(self.QLW_rel3)        
+        self.QGBox_rel3.setLayout(self.vbox_rel3)
+        self.QHL_rel.addWidget(self.QGBox_rel3)
+        
+        self.layout_Search = QVBoxLayout()
+        self.layout_Search.addWidget(self.QGBox_rel0)
 
+        ## FINAL PART ##
         #Layout principal : création et peuplement
         self.layout_general = QVBoxLayout()
         self.layout_general.addLayout(self.layoutH_radio)
-        self.layout_general.addWidget(self.onglets)
+        self.layout_general.addWidget(self.layout_onglets)
+        self.layout_general.addLayout(self.layout_Search)
         self.layout_general.addLayout(self.layoutH_boutons)
         self.setLayout(self.layout_general)
 
-    def liste4(self):
-#        cmd_working_dirs_creation(self) # create rel/ref directories - to be done later
-#        print "liste 4 Get Choice"
-        
-        if not os.path.exists(self.working_dir_rel):
-            os.chdir(self.working_dir_base) # going to base folder
-            print "liste 4 - Creation of (%s) folder" % str(self.lineedit1.text()[6:])
-            os.makedirs(str(self.lineedit1.text()[6:]))
-
-        os.chdir(self.working_dir_rel)   # Change current working directory
-        if not os.path.exists(self.working_dir_ref):
-            print "liste 4 - Creation of (%s) folder" % str(self.lineedit3.text()[6:])
-            os.makedirs(str(self.lineedit3.text()[6:]))
-#        list_search(self) # va chercher tous les fichiers
-        to_transmit = [str("lineedit1"), str("lineedit3"), self.rel_list_0, self.ref_list_0] # [str(self.lineedit1.text()), str(self.lineedit3.text()), self.rel_list, self.ref_list] # temporaly removed
-        self.getChoice_update(to_transmit)
-              
-    def getChoice_update(self, to_transmit):
-        from operator import itemgetter
-        """Lance la deuxième fenêtre"""
-        self.getChoice = GetChoice()
-        print "\ngetChoice_update - to_transmit : ", to_transmit
-        
-        self.rel_list_mod = []
-        self.ref_list_mod = []
-        self.rel_list_mod2 = []
-        self.ref_list_mod2 = []
-    
-        # en cas de signal "fermeturegetChoice()" reçu de self.getChoice => exécutera clienchoice 
-        self.connect(self.getChoice, SIGNAL("fermeturegetChoice(PyQt_PyObject)"), self.clientchoice) 
-        # la deuxième fenêtre sera 'modale' (la première fenêtre sera inactive)
-        self.getChoice.setWindowModality(QtCore.Qt.ApplicationModal)
-        # appel de la deuxième fenêtre
-        self.getChoice.show()
-
-    def clientchoice(self, x):
-        """affiche le résultat x transmis par le signal à l'arrêt de la deuxième fenêtre"""
-        print "recuperation = ", x, "\n" # to be removed
-        self.my_choice_rel_0 = x[0]
-        self.my_choice_ref_0 = x[1]
-        self.my_choice_rel_1 = x[2]
-        self.my_choice_ref_1 = x[3]
-        tmp = self.trUtf8(self.texte) 
-        tmp += "<br /><strong>Release : </strong>"
-        if ( self.my_choice_rel_0 ) :
-            print "clientchoice : self.my_choice_rel_0 : %s " % self.my_choice_rel_0
-            tmp += self.my_choice_rel_0 # 
-            tmp += '/' + self.my_choice_rel_1 # 
-        tmp += "<br /><strong>Reference : </strong>"
-        if ( self.my_choice_ref_0 ) :
-            print "clientchoice : self.my_choice_ref_0 : %s " % self.my_choice_ref_0
-            tmp += str(self.my_choice_ref_0) # 
-            tmp += '/' + self.my_choice_ref_1 # 
-        self.labelResume.setText(tmp)
-        QtCore.QCoreApplication.processEvents()
-#        self.bouton5.setEnabled(False) # to be seen later
-#        if ( self.my_choice_rel ) :
-#            if ( self.my_choice_ref ) :
-#                self.bouton5.setEnabled(True)
-
-    def buttons_relClicked(self):
-        i = 0
-        k = 0
-        for items in self.rel_list_mod2:
-            j = 0
-            items = (items[0], items[1], items[2])
-            for items2 in items:
-                if ( j == 1 ):
-                    if self.buttons_rel[i].isChecked():
-                        self.my_choice_rel = self.rel_list_mod2[k]
-                        self.choice_rel = self.rel_list_mod2[k]
-                j += 1
-                i += 1
-            k += 1
-        QtCore.QCoreApplication.processEvents()
-
-    def buttons_refClicked(self):
-        i = 0
-        k = 0
-        for items in self.ref_list_mod2:
-            j = 0
-            items = (items[0], items[1], items[2])
-            for items2 in items:
-                if ( j == 1 ):
-                    if self.buttons_ref[i].isChecked():
-                        self.my_choice_ref = self.ref_list_mod2[k]
-                        self.choice_ref = self.ref_list_mod2[k]
-                j += 1
-                i += 1
-            k += 1
-        QtCore.QCoreApplication.processEvents()
-                                
     def radio11Clicked(self):
         if self.radio11.isChecked():
             self.QGBox31.setVisible(True)
@@ -302,3 +278,76 @@ class ovalGui(QWidget):
             self.check38.setChecked(False)
         QtCore.QCoreApplication.processEvents() 
 
+    def ItemRelRefClicked1(self):
+        self.QGBox_rel2.setTitle(self.QLW_rel1.currentItem().text())        
+        if self.QGBox_rel0.title() == "Reference list":
+            print "reference"
+            self.my_choice_ref_0 = self.QLW_rel1.currentItem().text()
+            print "ItemRefClicked1 : self.my_choice_ref_0 : %s " % self.my_choice_ref_0
+            self.ref_list_0 = sub_releases(list_search_1(self.my_choice_ref_0))
+            self.QLW_rel2.clear()
+            self.QLW_rel3.clear()
+        else:
+            print "release"
+            self.my_choice_rel_0 = self.QLW_rel1.currentItem().text()
+            print "ItemRelRefClicked1 : self.my_choice_rel_0 : %s " % self.my_choice_rel_0
+            self.rel_list_0 = sub_releases(list_search_1(self.my_choice_rel_0))
+            # tester si =0
+            self.QLW_rel2.clear()
+            self.QLW_rel3.clear()
+            
+        for it in self.rel_list_0:
+            item = QListWidgetItem("%s" % it)
+            self.QLW_rel2.addItem(item)
+        
+    def ItemRelRefClicked2(self):
+        rel_text = self.QLW_rel2.currentItem().text() + " DataSets"
+        self.QGBox_rel3.setTitle(rel_text)
+        if self.QGBox_rel0.title() == "Reference list":
+            print "reference"
+            self.my_choice_ref_1 = self.QLW_rel2.currentItem().text()
+            print "ItemRefClicked2 : self.my_choice_ref_1 : %s " % self.my_choice_ref_1
+            tmp = "Reference : " + self.my_choice_ref_1
+            self.labelCombo2.setText(tmp)
+            self.ref_list_1 = sub_releases2(str(self.my_choice_ref_1), list_search_1(self.my_choice_ref_1))
+        else:
+            print "release"
+            self.my_choice_rel_1 = self.QLW_rel2.currentItem().text()
+            print "ItemRelRefClicked2 : self.my_choice_rel_1 : %s " % self.my_choice_rel_1
+            tmp = "Release : " + self.my_choice_rel_1
+            self.labelCombo1.setText(tmp)
+            self.rel_list_1 = sub_releases2(str(self.my_choice_rel_1), list_search_1(self.my_choice_rel_1))
+            self.QLW_rel3.clear()
+ 
+        for it in self.rel_list_1:
+            item = QListWidgetItem("%s" % it)
+            self.QLW_rel3.addItem(item)
+        
+    def ItemRelRefClicked3(self): # to be used later
+        #self.QGBox_rel4.setTitle(self.QLW_rel3.currentItem().text())
+        #self.my_choice_rel_1 = self.QLW_rel2.currentItem().text()
+        print "ItemRelRefClicked3 : self.my_choice_rel_1 : %s " % self.QLW_rel3.currentItem().text()
+        
+    def Next_Choice(self):
+        print "Next_Choice: "
+        self.bouton_Previous.setEnabled(True)
+        self.bouton_Next.setEnabled(False)
+        self.QGBox_rel0.setTitle("Reference list")
+        self.QLW_rel1.clear()
+        self.QLW_rel2.clear()
+        self.QLW_rel3.clear()
+        for it in self.releasesList_0:
+            item = QListWidgetItem("%s" % it)
+            self.QLW_rel1.addItem(item)
+
+    def Previous_Choice(self):
+        print "Previous_Choice: "
+        self.bouton_Previous.setEnabled(False)
+        self.bouton_Next.setEnabled(True)
+        self.QGBox_rel0.setTitle("Release list")
+        self.QLW_rel1.clear()
+        self.QLW_rel2.clear()
+        self.QLW_rel3.clear()
+        for it in self.releasesList_0:
+            item = QListWidgetItem("%s" % it)
+            self.QLW_rel1.addItem(item)
